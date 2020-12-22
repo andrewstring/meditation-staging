a4877dc8da2abfd09632105ab54c5e2f82909a07
Andrew Stringfellow <abreezemail@gmail.com>
2020-12-21 20:11:11 -0500

    added secret key from heroku

--- main/config.py
+++ main/config.py
@@ -3,7 +3,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     #secret keys
-    SECRET_KEY = os.urandom(24);
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     SECURITY_PASSWORD_SALT = 'secret-salt'
 
     SQLALCHEMY_DATABASE_URI = 'postgres://gmgskvzrhawssf:529c332f2de37b3b2e968a914ae398bb2b9a744eceb6ed5ad3da41a8d49ef058@ec2-54-237-135-248.compute-1.amazonaws.com:5432/dmnha41oe8n1h'
