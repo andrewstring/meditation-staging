dd15d5b05c5b83dafcc86c93ba4b86a9f80d8d46
Andrew Stringfellow <abreezemail@gmail.com>
2020-12-21 21:08:17 -0500

    sqlalchemy fix

--- main/config.py
+++ main/config.py
@@ -6,7 +6,7 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SECURITY_PASSWORD_SALT = 'secret-salt'
 
-    DATABASE_URL = 'postgres://gmgskvzrhawssf:529c332f2de37b3b2e968a914ae398bb2b9a744eceb6ed5ad3da41a8d49ef058@ec2-54-237-135-248.compute-1.amazonaws.com:5432/dmnha41oe8n1h'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     #mail
